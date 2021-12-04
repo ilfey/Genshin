b994b6c908a3f19a1866fd8b328ba7a43a2497b2
Josty <josty.qweenq@gmail.com>
2021-12-04 14:05:10 +0700

    initial commit

---
+++ app/src/main/java/com/example/genshin/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.genshin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
