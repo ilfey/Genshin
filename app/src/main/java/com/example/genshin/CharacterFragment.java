97cb699b2ffccbd31a94f0dda3f6b67573d37f92
Josty <josty.qweenq@gmail.com>
2021-12-07 21:46:57 +0700

    added character fragment

---
+++ app/src/main/java/com/example/genshin/CharacterFragment.java
@@ -0,0 +1,22 @@
+package com.example.genshin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CharacterFragment extends Fragment {
+    public CharacterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_character, container, false);
+    }
+}
