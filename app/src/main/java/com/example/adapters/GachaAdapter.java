74bd1af45b555700677f2670aeeaa6b651652df6
unknown <mad@LAB403-K3.ttit.local>
2022-02-02 14:01:04 +0700

    added RecyclerView for gacha fragment

---
+++ app/src/main/java/com/example/adapters/GachaAdapter.java
@@ -0,0 +1,60 @@
+package com.example.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.genshin.R;
+import com.example.models.GachaModel;
+
+import java.util.List;
+
+public class GachaAdapter extends RecyclerView.Adapter<GachaAdapter.GachaViewHolder>{
+
+    Context ctx;
+    private final List<GachaModel> models;
+
+    public GachaAdapter(Context ctx, List<GachaModel> models) {
+        this.ctx = ctx;
+        this.models = models;
+    }
+
+    @NonNull
+    @Override
+    public GachaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(ctx).inflate(R.layout.model_gacha, parent, false);
+        return new GachaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GachaViewHolder holder, int position) {
+        holder.icon.setImageResource(models.get(position).getIcon());
+        holder.title.setText(models.get(position).getTitle());
+        holder.skills.setText(models.get(position).getSkills());
+    }
+
+    @Override
+    public int getItemCount() {
+        return models.size();
+    }
+
+    public final class GachaViewHolder extends RecyclerView.ViewHolder {
+        ImageView icon;
+        TextView title;
+        TextView skills;
+
+        public GachaViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            icon = (ImageView) itemView.findViewById(R.id.model_gacha_icon);
+            title = (TextView) itemView.findViewById(R.id.model_gacha_title);
+            skills = (TextView) itemView.findViewById(R.id.model_gacha_skills);
+        }
+    }
+}
--- app/src/main/java/com/example/genshin/GachaFragment.java
+++ app/src/main/java/com/example/genshin/GachaFragment.java
@@ -2,34 +2,51 @@ package com.example.genshin;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import com.example.adapters.CharacterAdapter;
+import com.example.adapters.GachaAdapter;
+import com.example.models.GachaModel;
+import com.example.models.MenuCharacter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GachaFragment extends Fragment {
 
-//    List<String> ArraySort = new ArrayList<String>();
-
-    public GachaFragment() {
-        // Required empty public constructor
-    }
+    RecyclerView gacha_recycler;
+    GachaAdapter gachaAdapter;
+    List<GachaModel> gachaModels = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gacha, container, false);
+        View view = inflater.inflate(R.layout.fragment_gacha, container, false);
 
-        /*ArraySort.add("От новых");
-        ArraySort.add("От старых");
-        ArraySort.add("По алфавиту");
-        ArraySort.add("С конца алфавита");*/
+        gacha_recycler = view.findViewById(R.id.gacha_recycler);
+        gachaAdapter = new GachaAdapter(getContext(), gachaModels);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        gacha_recycler.setLayoutManager(layoutManager);
+        gacha_recycler.setAdapter(gachaAdapter);
 
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        gachaModels.add(new GachaModel(1, R.mipmap.eola, "Эола", "Эола 5*\nРозария 4*\nНоэль 4*\nБеннет 4*"));
+        gachaModels.add(new GachaModel(2, R.mipmap.eola, "Эола", "Эола 5*\nРозария 4*\nНоэль 4*\nБеннет 4*"));
+        gachaModels.add(new GachaModel(3, R.mipmap.eola, "Эола", "Эола 5*\nРозария 4*\nНоэль 4*\nБеннет 4*"));
     }
 }
