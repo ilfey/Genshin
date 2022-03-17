05bb175e939f1a8066bd5d28c595401632a16bdc
Josty Qweenq <josty.qweenq@gmail.com>
2022-03-17 10:51:56 +0700

    switched to group

--- app/src/main/java/com/example/genshin/CharacterActivity.java
+++ app/src/main/java/com/example/genshin/CharacterActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Group;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.annotation.SuppressLint;
@@ -40,14 +41,7 @@ public class CharacterActivity extends AppCompatActivity {
     private EditText region;
     private EditText affiliation;
     private EditText dest;
-    private EditText rarityText;
-    private EditText weaperonText;
-    private EditText eyeText;
-    private EditText fullnameText;
-    private EditText sexText;
-    private EditText birthdayText;
-    private EditText regionText;
-    private EditText affiliationText;
+    private Group editGroup;
     private ConstraintLayout destLayout;
     private List<CharacterEntry> models;
     private int position = 0;
@@ -91,7 +85,6 @@ public class CharacterActivity extends AppCompatActivity {
                 loadContent();
             }
 
-
             @Override
             public void onSwipeRight() {
                 position--;
@@ -101,9 +94,11 @@ public class CharacterActivity extends AppCompatActivity {
                 loadContent();
             }
         });
+
         ctx = this;
         character_logo = findViewById(R.id.character_logo);
         models = ((GenshinApp) getApplication()).characters;
+
         title = findViewById(R.id.title);
         rarity = findViewById(R.id.rarity);
         weaperon = findViewById(R.id.weapon);
@@ -115,25 +110,15 @@ public class CharacterActivity extends AppCompatActivity {
         affiliation = findViewById(R.id.affiliation);
         dest = findViewById(R.id.dest);
 
-        rarityText = findViewById(R.id.rarity_text);
-        weaperonText = findViewById(R.id.weapon_text);
-        eyeText = findViewById(R.id.eye_text);
-        fullnameText = findViewById(R.id.fullname_text);
-        sexText = findViewById(R.id.sex_text);
-        birthdayText = findViewById(R.id.birthday_text);
-        regionText = findViewById(R.id.region_text);
-        affiliationText = findViewById(R.id.affiliation_text);
-        destLayout = findViewById(R.id.dest_layout);
+        editGroup = findViewById(R.id.edit_group);
 
-        rarity.addTextChangedListener(new TextChanged(rarityText));
-        weaperon.addTextChangedListener(new TextChanged(weaperonText));
-        eye.addTextChangedListener(new TextChanged(eyeText));
-        fullname.addTextChangedListener(new TextChanged(fullnameText));
-        sex.addTextChangedListener(new TextChanged(sexText));
-        birthday.addTextChangedListener(new TextChanged(birthdayText));
-        region.addTextChangedListener(new TextChanged(regionText));
-        affiliation.addTextChangedListener(new TextChanged(affiliationText));
-        dest.addTextChangedListener(new TextChanged(destLayout));
+        int[] editIds = editGroup.getReferencedIds();
+
+        for (int id : editIds) {
+            EditText view = findViewById(id);
+
+            view.addTextChangedListener(new TextChanged(view));
+        }
 
         refresh = findViewById(R.id.refresh);
         refresh.setColorSchemeResources(R.color.primary);
@@ -259,6 +244,13 @@ public class CharacterActivity extends AppCompatActivity {
         String affiliationString = models.get(position).getAffiliation();
         String destString = models.get(position).getDesk();
 
+        CharacterEntry entry = models.get(position);
+
+        /*for (int id : editGroup.getReferencedIds()){
+            EditText view = findViewById(id);
+            view.setText();
+        }*/
+
         title.setText(titleString);
 
         rarity.setText(rarityString);
@@ -273,16 +265,10 @@ public class CharacterActivity extends AppCompatActivity {
     }
 
     private void changeEditable(boolean editText){
-        title.setEnabled(editText);
-        rarity.setEnabled(editText);
-        weaperon.setEnabled(editText);
-        eye.setEnabled(editText);
-        fullname.setEnabled(editText);
-        sex.setEnabled(editText);
-        birthday.setEnabled(editText);
-        region.setEnabled(editText);
-        affiliation.setEnabled(editText);
-        dest.setEnabled(editText);
+        for (int id : editGroup.getReferencedIds()){
+            EditText view = findViewById(id);
+            view.setEnabled(editText);
+        }
     }
 
     public void showToast(String msg){
